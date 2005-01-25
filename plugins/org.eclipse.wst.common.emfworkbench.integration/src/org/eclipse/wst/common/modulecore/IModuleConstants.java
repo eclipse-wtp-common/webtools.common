68ccb1b4e263d236b473cdca58a13e8de0093ec2
cbridgha <cbridgha>
2005-01-25 17:55:30 +0000

    [flexable project support -BLB]: <new ModuleCoreNature API --IN PROGRESS-> + Committed for M3

---
+++ plugins/org.eclipse.wst.common.emfworkbench.integration/src/org/eclipse/wst/common/modulecore/IModuleConstants.java
@@ -0,0 +1,19 @@
+/*
+ * Created on Jan 25, 2005
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package org.eclipse.wst.common.modulecore;
+
+/**
+ * @author blancett
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public interface IModuleConstants {
+	
+	public final static String WTPMODULE_URI = ".wtpModule"; //$NON-NLS-1$
+
+}
