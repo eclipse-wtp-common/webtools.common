3900fac061e12c55597389c061bfe719f6203db0
cbridgha <cbridgha>
2005-02-07 21:07:22 +0000

    [nobug]: Continuing development of the Flexible Project Structure. Committed for MDE.

---
+++ tests/org.eclipse.wst.common.tests/commontests/org/eclipse/wst/common/tests/PlatformModuleURLTest.java
@@ -0,0 +1,46 @@
+/*
+ * Created on Jan 21, 2005
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package org.eclipse.wst.common.tests;
+
+import java.net.URL;
+import java.net.URLConnection;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import org.eclipse.wst.common.modulecore.impl.PlatformURLModuleConnection;
+
+public class PlatformModuleURLTest extends TestCase {
+
+    public PlatformModuleURLTest(String name) {
+        super(name);
+    }
+    
+    
+    public static Test suite() {
+        return new TestSuite(PlatformModuleURLTest.class);
+    }
+    
+    /* (non-Javadoc)
+     * @see junit.framework.TestCase#setUp()
+     */
+    protected void setUp() throws Exception { 
+        super.setUp();
+        PlatformURLModuleConnection.startup();
+    }
+        
+    /**
+     * 
+     */
+    public void testURLResolve() throws Exception {
+        URL url = new URL("platform:/module:/MyModule/META-INF/ejb-jar.xml");
+        URLConnection conx = url.openConnection();
+        System.out.println(conx.getURL());
+
+    }
+}
