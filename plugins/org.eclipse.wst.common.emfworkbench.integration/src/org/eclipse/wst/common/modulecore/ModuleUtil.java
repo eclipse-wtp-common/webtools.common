e85487bd82c54bb04e22f6575e7747ae9c8abd13
cbridgha <cbridgha>
2005-02-09 18:01:39 +0000

    [flexable project support -BLB]: <new Flexable Project API --IN PROGRESS-> + Committed for M3

--- plugins/org.eclipse.wst.common.emfworkbench.integration/src/org/eclipse/wst/common/modulecore/ModuleUtil.java
+++ plugins/org.eclipse.wst.common.emfworkbench.integration/src/org/eclipse/wst/common/modulecore/ModuleUtil.java
@@ -1,9 +1,4 @@
-/*
- * Created on Feb 7, 2005
- *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
- */
+
 package org.eclipse.wst.common.modulecore;
 
 import org.eclipse.core.resources.IProject;
@@ -19,7 +14,8 @@ public class ModuleUtil implements IModuleConstants {
     public static boolean isFlexableProject(IProject project) {
         IProjectNature nature = null;
         try {
-            nature = project.getNature(MODULE_NATURE_ID);
+            if (project.isAccessible())
+             nature = project.getNature(MODULE_NATURE_ID);
         } catch (CoreException e) {
             Logger.getLogger().write(e);
         }
