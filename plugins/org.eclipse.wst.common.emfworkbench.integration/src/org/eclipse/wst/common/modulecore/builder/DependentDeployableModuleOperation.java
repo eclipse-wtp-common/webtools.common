14f511d023b616417da9cb20acd6876f4a004000
cbridgha <cbridgha>
2005-02-09 19:41:14 +0000

    [nobug] Changes to builder code for .wtpmodules. Committing for DFH

--- plugins/org.eclipse.wst.common.emfworkbench.integration/src/org/eclipse/wst/common/modulecore/builder/DependentDeployableModuleOperation.java
+++ plugins/org.eclipse.wst.common.emfworkbench.integration/src/org/eclipse/wst/common/modulecore/builder/DependentDeployableModuleOperation.java
@@ -10,24 +10,41 @@
  *******************************************************************************/
 package org.eclipse.wst.common.modulecore.builder;
 
+import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.wst.common.frameworks.internal.operations.WTPOperation;
+import org.eclipse.wst.common.internal.emfworkbench.integration.EMFWorkbenchEditPlugin;
 import org.eclipse.wst.common.modulecore.WorkbenchModule;
 import org.eclipse.wst.common.modulecore.impl.UnresolveableURIException;
 import org.eclipse.wst.common.modulecore.util.ModuleCore;
+import org.eclipse.wst.common.modulecore.util.ZipFileExporter;
 
 public class DependentDeployableModuleOperation extends WTPOperation {
+	private static String ERROR_EXPORTING_MSG = "Zip Error Message"; //$NON-NLS-1$
     private DependentDeployableModuleDataModel depDataModel = null;
+    private ZipFileExporter exporter = null;
+
+	private List errorTable = new ArrayList(1); //IStatus
+	private boolean useCompression = true;
+	private boolean createLeadupStructure = true;
+	private boolean generateManifestFile = false;
+	private IProgressMonitor monitor;
     /**
      * @param operationDataModel
      */
@@ -39,6 +56,7 @@ public class DependentDeployableModuleOperation extends WTPOperation {
      * @see org.eclipse.wst.common.frameworks.internal.operations.WTPOperation#execute(org.eclipse.core.runtime.IProgressMonitor)
      */
     protected void execute(IProgressMonitor monitor) throws CoreException, InvocationTargetException, InterruptedException {
+		this.monitor = monitor; 
         IPath absoluteOutputContainer = getAbsoluteOutputContainer();    
        	// create output container folder if it does not exist
 		IFolder outputContainerFolder = createFolder(absoluteOutputContainer);	
@@ -53,11 +71,23 @@ public class DependentDeployableModuleOperation extends WTPOperation {
 			sourceResource.copy(absoluteInputContainer, true, new NullProgressMonitor());
 		} else {
 		    String zipName = getZipFileName();
-		    //commona
-		
+		    zipAndCopyResource(getResource(absoluteInputContainer), absoluteInputContainer.append(zipName).toString());
 		}    
     }
 	/**
+     * @param resource
+     * @param zipName
+     * @return
+     */
+    private void zipAndCopyResource(IResource resource, String zipNameDestination ) throws InterruptedException{
+        try {
+            exporter = new ZipFileExporter(zipNameDestination,	true, true);
+            exportResource(resource);
+		    exporter.finished();
+        } catch (IOException ioEx) {
+        }
+    }
+    /**
      * @return
      */
     private IPath getAbsoluteOutputContainer() {
@@ -123,5 +153,106 @@ public class DependentDeployableModuleOperation extends WTPOperation {
 		if (!folder.exists())
 			folder.create(true, true, new NullProgressMonitor());
 		return folder;
+	}
+	/**
+	 *  Export the passed resource to the destination .zip. Export with
+	 * no path leadup
+	 *
+	 *  @param resource org.eclipse.core.resources.IResource
+	 */
+	protected void exportResource(IResource resource)
+		throws InterruptedException {
+		exportResource(resource, 1);
+	}
+
+	/**
+	 *  Export the passed resource to the destination .zip
+	 *
+	 *  @param resource org.eclipse.core.resources.IResource
+	 *  @param depth - the number of resource levels to be included in
+	 *   				the path including the resourse itself.
+	 */
+	protected boolean exportResource(IResource resource, int leadupDepth)
+		throws InterruptedException {
+		if (!resource.isAccessible())
+			return false;
+
+		if (resource.getType() == IResource.FILE) {
+			return writeResource(resource, leadupDepth);
+		} else {
+			IResource[] children = null;
+
+			try {
+				children = ((IContainer) resource).members();
+			} catch (CoreException e) {
+				// this should never happen because an #isAccessible check is done before #members is invoked
+				addError(format(ERROR_EXPORTING_MSG, new Object[] { resource.getFullPath()}), e); //$NON-NLS-1$
+			}
+			
+			boolean writeFolder = true;
+			for (int i = 0; i < children.length; i++) {
+				writeFolder = !exportResource(children[i], leadupDepth + 1) && writeFolder;
+			}
+			if (writeFolder) {
+				writeResource(resource, leadupDepth);
+			}
+			return true;
+
+		}
+	}
+	
+	private boolean writeResource(IResource resource, int leadupDepth) throws InterruptedException {
+		if (resource.isDerived())
+			return false;
+		String destinationName;
+		IPath fullPath = resource.getFullPath();
+		if (createLeadupStructure)
+			destinationName = fullPath.makeRelative().toString();
+		else
+			destinationName =
+				fullPath
+					.removeFirstSegments(
+						fullPath.segmentCount() - leadupDepth)
+					.toString();
+		monitor.subTask(destinationName);
+
+		try {
+			if (resource.getType() == IResource.FILE)
+				exporter.write((IFile) resource, destinationName);
+			else 
+				exporter.writeFolder(destinationName);
+		} catch (IOException e) {
+			addError(format(ERROR_EXPORTING_MSG, //$NON-NLS-1$
+			new Object[] {
+				resource.getFullPath().makeRelative(),
+				e.getMessage()}),
+				e);
+			return false;
+		} catch (CoreException e) {
+			addError(format(ERROR_EXPORTING_MSG, //$NON-NLS-1$
+			new Object[] {
+				resource.getFullPath().makeRelative(),
+				e.getMessage()}),
+				e);
+			return false;
+		}
+
+		monitor.worked(1);
+		return true;
+	}
+	/**
+	 * @param ERROR_EXPORTING_MSG
+	 * @param objects
+	 * @return
+	 */
+	private String format(String pattern, Object[] arguments) {
+		return MessageFormat.format(pattern, arguments);
+	}
+	/**
+	 * Add a new entry to the error table with the passed information
+	 */
+	protected void addError(String message, Throwable e) {
+		errorTable.add(
+			new Status(IStatus.ERROR, EMFWorkbenchEditPlugin.ID, 0, message, e));
 	}
 }
