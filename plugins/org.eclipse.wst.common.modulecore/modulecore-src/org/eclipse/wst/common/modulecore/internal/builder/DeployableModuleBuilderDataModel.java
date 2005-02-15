459c18d084e3fcd818bd3b8371a3ace8ad8ff760
cbridgha <cbridgha>
2005-02-15 14:33:14 +0000

    organized imports

--- plugins/org.eclipse.wst.common.modulecore/modulecore-src/org/eclipse/wst/common/modulecore/internal/builder/DeployableModuleBuilderDataModel.java
+++ plugins/org.eclipse.wst.common.modulecore/modulecore-src/org/eclipse/wst/common/modulecore/internal/builder/DeployableModuleBuilderDataModel.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.core.resources.IFolder;
-import org.eclipse.emf.common.util.URI;
 import org.eclipse.wst.common.frameworks.operations.WTPOperation;
 import org.eclipse.wst.common.frameworks.operations.WTPOperationDataModel;
 import org.eclipse.wst.common.modulecore.ModuleCore;
