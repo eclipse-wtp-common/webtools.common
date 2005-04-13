191dd56fd1eb685d5b9e984fe8000dad3812295a
cbridgha <cbridgha>
2005-04-13 02:07:01 +0000

    [nobug] Fixing up component.xml

---
+++ tests/org.eclipse.wst.common.tests/frameworktests/org/eclipse/wst/common/frameworks/componentcore/tests/IVirtualReferenceAPITest.java
@@ -0,0 +1,42 @@
+package org.eclipse.wst.common.frameworks.componentcore.tests;
+
+import junit.framework.TestCase;
+
+public class IVirtualReferenceAPITest extends TestCase {
+
+	public static void main(String[] args) {
+	}
+
+	public void testCreate() {
+		//TODO Implement create().
+	}
+
+	public void testSetRuntimePath() {
+		//TODO Implement setRuntimePath().
+	}
+
+	public void testGetRuntimePath() {
+		//TODO Implement getRuntimePath().
+	}
+
+	public void testSetDependencyType() {
+		//TODO Implement setDependencyType().
+	}
+
+	public void testGetDependencyType() {
+		//TODO Implement getDependencyType().
+	}
+
+	public void testExists() {
+		//TODO Implement exists().
+	}
+
+	public void testGetEnclosingComponent() {
+		//TODO Implement getEnclosingComponent().
+	}
+
+	public void testGetReferencedComponent() {
+		//TODO Implement getReferencedComponent().
+	}
+
+}
