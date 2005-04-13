8f2fe3be93b2cc546b546a48e242072922aafe0d
cbridgha <cbridgha>
2005-04-13 02:20:02 +0000

    [nobug] Fixing up component.xml

---
+++ plugins/org.eclipse.wst.common.frameworks/src/org/eclipse/wst/common/frameworks/datamodel/properties/IFlexibleProjectCreationDataModelProperties.java
@@ -0,0 +1,12 @@
+package org.eclipse.wst.common.frameworks.datamodel.properties;
+
+import org.eclipse.wst.common.frameworks.datamodel.IDataModelProperties;
+
+
+public interface IFlexibleProjectCreationDataModelProperties extends IDataModelProperties {
+   public static final String PROJECT_NAME = "IFlexibleProjectCreationDataModelProperties.PROJECT_NAME"; //$NON-NLS-1$
+   
+    public static final String PROJECT_LOCATION = "IFlexibleProjectCreationDataModelProperties.PROJECT_LOCATION"; //$NON-NLS-1$
+    
+    public static final String NESTED_MODEL_PROJECT_CREATION = "IFlexibleProjectCreationDataModelProperties.NESTED_MODEL_PROJECT_CREATION"; //$NON-NLS-1$
+}
