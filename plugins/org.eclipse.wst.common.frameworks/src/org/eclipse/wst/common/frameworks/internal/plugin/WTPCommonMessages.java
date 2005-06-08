65702532a54c050d143a68d1c3c010f67197a6fb
vbhadrir <vbhadrir>
2005-06-08 17:40:36 +0000

    [nobug] export work.  Committing for DFH

--- plugins/org.eclipse.wst.common.frameworks/src/org/eclipse/wst/common/frameworks/internal/plugin/WTPCommonMessages.java
+++ plugins/org.eclipse.wst.common.frameworks/src/org/eclipse/wst/common/frameworks/internal/plugin/WTPCommonMessages.java
@@ -44,4 +44,6 @@ public class WTPCommonMessages {
 	public static final String ERR_INVALID_CHARS = "35"; //$NON-NLS-1$ 
 	public static final String DUPLICATE_COMPONENT_NAME="36";//$NON-NLS-1$ 
 	public static final String MODULE_NOT_SUPPORTED="37";//$NON-NLS-1$ 
+    
+    public static final String MODULE_EXISTS_ERROR = "38"; //$NON-NLS-1$
 }
