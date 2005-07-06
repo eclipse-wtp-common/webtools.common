5c575b8a2dd7b473d90abbbe2bc0365c3e6b7ded
vbhadrir <vbhadrir>
2005-07-06 20:00:13 +0000

    [102797] proxy resolution for extensions: committed for JL

--- plugins/org.eclipse.wst.common.modulecore/modulecore-src/org/eclipse/wst/common/componentcore/internal/impl/MappedComponentXMIHelper.java
+++ plugins/org.eclipse.wst.common.modulecore/modulecore-src/org/eclipse/wst/common/componentcore/internal/impl/MappedComponentXMIHelper.java
@@ -2,72 +2,65 @@ package org.eclipse.wst.common.componentcore.internal.impl;
 
 import java.util.Map;
 
-import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.URIConverterImpl;
 import org.eclipse.emf.ecore.xmi.XMLResource;
-import org.eclipse.jem.util.emf.workbench.ProjectResourceSet;
 import org.eclipse.jem.util.emf.workbench.WorkbenchResourceHelperBase;
-import org.eclipse.wst.common.componentcore.UnresolveableURIException;
-import org.eclipse.wst.common.componentcore.internal.ComponentResource;
-import org.eclipse.wst.common.componentcore.internal.StructureEdit;
+import org.eclipse.wst.common.componentcore.ComponentCore;
+import org.eclipse.wst.common.componentcore.resources.IVirtualComponent;
+import org.eclipse.wst.common.componentcore.resources.IVirtualFile;
+import org.eclipse.wst.common.componentcore.resources.IVirtualResource;
 import org.eclipse.wst.common.internal.emf.resource.MappedXMIHelper;
+import org.eclipse.wst.common.internal.emfworkbench.WorkbenchResourceHelper;
 
 public class MappedComponentXMIHelper extends MappedXMIHelper {
 
 	public MappedComponentXMIHelper(XMLResource resource, Map prefixesToURIs) {
 		super(resource, prefixesToURIs);
-		// TODO Auto-generated constructor stub
 	}
 
 	public URI resolve(URI relative, URI base) {
-
 		URI resolved = null;
+		if (!isMapped(relative))
+			resolved = resolveURIFromComponent(relative,base);
+		else
+			resolved = relative;
+		
+		return resolved == null ? super.resolve(relative, base) : resolved;
+	}
+	
+	private boolean isMapped(URI relative) {
 		boolean isMapped = false;
 		ResourceSet set = getResource().getResourceSet();
 		if (set != null) {
-			URI localresourceURI = null;
+			URI localresourceURI = relative;
 			if (relative.hasFragment())
 				localresourceURI = relative.trimFragment();
-			else
-				localresourceURI = relative;
-			isMapped = !(((URIConverterImpl.URIMap) set.getURIConverter().getURIMap()).getURI(localresourceURI).equals(localresourceURI));
+			isMapped = !((URIConverterImpl.URIMap) set.getURIConverter().getURIMap()).getURI(localresourceURI).equals(localresourceURI);
 		}
-		
-		if (!isMapped) {
-			String compName = null;
-			ComponentResource theResource = null;
-			URI compPath = relative;
-			StructureEdit se = null;
-			try {
-				se = StructureEdit.getStructureEditForRead(getProject());
-				theResource = se.findResourcesBySourcePath(WorkbenchResourceHelperBase.getPathInProject(getProject(),new Path(WorkbenchResourceHelperBase.getNonPlatformURIString(base))))[0];
-				if (theResource != null) {
-					compPath = URI.createURI((theResource.getComponent().findResourcesByRuntimePath(new Path(relative.path()))[0]).getSourcePath().toOSString());
-					if (set != null) {
-					resolved = set.getURIConverter().normalize(compPath);
-					}
-				}
-			} catch (UnresolveableURIException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} finally {
-				if (se != null)
-					se.dispose();
-			}
-			
-			
-		} else {
-			resolved = relative;
-		}
-		return resolved == null ? super.resolve(relative, base) : resolved;
+		return isMapped;
+	}
 	
+	private URI resolveURIFromComponent(URI relative, URI base) {
+		IVirtualComponent component = getComponent(base);
+		if (component != null) {
+			IVirtualFile virtualFile = component.getRootFolder().getFile(new Path(relative.toString()));
+			if (virtualFile != null) {
+				IPath resolvingPath = virtualFile.getWorkspaceRelativePath();
+				if (resolvingPath !=null) 
+					return URI.createPlatformResourceURI(resolvingPath.toString());
+			}
+		}
+		return null;
 	}
-
-	private IProject getProject() {
-		return ((ProjectResourceSet)getResource().getResourceSet()).getProject();
+	
+	private IVirtualComponent getComponent(URI base) {
+		IVirtualResource[] virtualResources = ComponentCore.createResources(WorkbenchResourceHelper.getFile(WorkbenchResourceHelperBase.getResource(base)));
+		if (virtualResources.length>0)
+			return virtualResources[0].getComponent();
+		return null;
 	}
-
 }
