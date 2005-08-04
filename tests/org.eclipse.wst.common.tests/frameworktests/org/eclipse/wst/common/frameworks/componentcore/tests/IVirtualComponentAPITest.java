edf297f849c08c98ca924a9b55d174a643515065
vbhadrir <vbhadrir>
2005-08-04 18:36:12 +0000

    [105470]commited for NA

--- tests/org.eclipse.wst.common.tests/frameworktests/org/eclipse/wst/common/frameworks/componentcore/tests/IVirtualComponentAPITest.java
+++ tests/org.eclipse.wst.common.tests/frameworktests/org/eclipse/wst/common/frameworks/componentcore/tests/IVirtualComponentAPITest.java
@@ -10,10 +10,14 @@
  *******************************************************************************/ 
 package org.eclipse.wst.common.frameworks.componentcore.tests;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
+import org.eclipse.wst.common.componentcore.internal.ComponentcoreFactory;
+import org.eclipse.wst.common.componentcore.internal.Property;
 import org.eclipse.wst.common.componentcore.resources.IVirtualFolder;
 import org.eclipse.wst.common.componentcore.resources.IVirtualResource;
 
@@ -76,6 +80,22 @@ public class IVirtualComponentAPITest extends BaseVirtualTest {
 	
 	public void testGetVersion(){
 		component.getVersion();
+	}
+	
+	public void testAddMetaProperty(){
+		Property prop = ComponentcoreFactory.eINSTANCE.createProperty();
+		prop.setName("Test1");
+		prop.setValue("Test1Value");
+		component.addMetaProperty(prop);
+	}
+	
+	public void testSetProperties(){
+		List newProps = new ArrayList();
+		Property prop = ComponentcoreFactory.eINSTANCE.createProperty();
+		prop.setName("Test2");
+		prop.setValue("Test2Value");
+		newProps.add(prop);
+		component.setMetaProperties(newProps);
 	}
 
 }
