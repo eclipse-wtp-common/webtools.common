433012fdd403964627fc44ca84135db609c3701d
david_williams <david_williams>
2005-08-24 21:10:37 +0000

    This commit was manufactured by cvs2svn to create tag 'M20050825_1101_b'.

--- plugins/org.eclipse.wst.common.frameworks/src/org/eclipse/wst/common/frameworks/internal/FlexibleJavaProjectPreferenceConstants.java
+++
@@ -1,5 +0,0 @@
-package org.eclipse.wst.common.frameworks.internal;
-
-public interface FlexibleJavaProjectPreferenceConstants {
-	public static final String ALLOW_MULTIPLE_MODULES = "ALLOW_MULTIPLE_MODULES_PER_PROJECT"; //$NON-NLS-1$
-}
