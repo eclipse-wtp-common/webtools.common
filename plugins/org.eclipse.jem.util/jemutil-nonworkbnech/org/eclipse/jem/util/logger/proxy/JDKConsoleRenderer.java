dea3fc7c146dc6b728fcd9df8084534cdebc055c
sgunturi <sgunturi>
2005-08-24 21:10:34 +0000

    83539 : Copyrights update for 2005.

--- plugins/org.eclipse.jem.util/jemutil-nonworkbnech/org/eclipse/jem/util/logger/proxy/JDKConsoleRenderer.java
+++ plugins/org.eclipse.jem.util/jemutil-nonworkbnech/org/eclipse/jem/util/logger/proxy/JDKConsoleRenderer.java
@@ -1,6 +1,6 @@
 /*******************************************************************************
- * Copyright (c) 2004 IBM Corporation and others.
- * All rights reserved. This program and the accompanying materials 
+ * Copyright (c) 2004, 2005 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
@@ -10,7 +10,7 @@
  *******************************************************************************/
 /*
  *  $RCSfile: JDKConsoleRenderer.java,v $
- *  $Revision: 1.2 $  $Date: 2005/02/15 23:05:54 $ 
+ *  $Revision: 1.3 $  $Date: 2005/08/24 21:10:34 $ 
  */
 package org.eclipse.jem.util.logger.proxy;
 
--- plugins/org.eclipse.jem.util/jemutil/org/eclipse/jem/util/TimerTests.java
+++ plugins/org.eclipse.jem.util/jemutil/org/eclipse/jem/util/TimerTests.java
@@ -1,6 +1,6 @@
 /*******************************************************************************
- * Copyright (c) 2004 IBM Corporation and others.
- * All rights reserved. This program and the accompanying materials 
+ * Copyright (c) 2004, 2005 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
@@ -10,7 +10,7 @@
  *******************************************************************************/
 /*
  *  $RCSfile: TimerTests.java,v $
- *  $Revision: 1.4 $  $Date: 2005/05/18 21:58:34 $ 
+ *  $Revision: 1.5 $  $Date: 2005/08/24 21:10:34 $ 
  */
 package org.eclipse.jem.util;
 
