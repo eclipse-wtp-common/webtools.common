a7ec613b3495391bb9d608bba6381fd344ff0ec7
vbhadrir <vbhadrir>
2005-08-24 19:51:46 +0000

    commited for NA

---
+++ plugins/org.eclipse.wst.common.frameworks/src/org/eclipse/wst/common/frameworks/internal/FlexibleJavaProjectPreferenceConstants.java
@@ -0,0 +1,5 @@
+package org.eclipse.wst.common.frameworks.internal;
+
+public interface FlexibleJavaProjectPreferenceConstants {
+	public static final String ALLOW_MULTIPLE_MODULES = "ALLOW_MULTIPLE_MODULES_PER_PROJECT"; //$NON-NLS-1$
+}
