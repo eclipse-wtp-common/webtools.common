7e5ec34ac8038e38ebac5c935c4671e02f4605a5
david_williams <david_williams>
2005-11-14 04:18:57 +0000

    This commit was manufactured by cvs2svn to create tag 'v20051113_2330'.

--- plugins/org.eclipse.wst.common.modulecore/modulecore-src/org/eclipse/wst/common/componentcore/internal/IComponentProjectMigrator.java
+++
@@ -1,8 +0,0 @@
-package org.eclipse.wst.common.componentcore.internal;
-
-import org.eclipse.core.resources.IProject;
-
-public interface IComponentProjectMigrator {
-	public void migrateProject(IProject project);
-
-}
