dd08e111cd92cb82ae22a8fb750cde566ef18bc4
jlanuti <jlanuti>
2006-03-02 02:51:41 +0000

    [126759] NPE fix on migration

--- plugins/org.eclipse.wst.common.modulecore/modulecore-src/org/eclipse/wst/common/componentcore/internal/operation/CreateReferenceComponentsOp.java
+++ plugins/org.eclipse.wst.common.modulecore/modulecore-src/org/eclipse/wst/common/componentcore/internal/operation/CreateReferenceComponentsOp.java
@@ -78,11 +78,16 @@ public class CreateReferenceComponentsOp extends AbstractDataModelOperation {
 	}
 
 	private boolean srcComponentContainsReference(IVirtualComponent sourceComp, IVirtualComponent comp) {
+		if (sourceComp == null || comp == null)
+			return false;
 		IVirtualReference[] existingReferences = sourceComp.getReferences();
-		for (int i = 0; i < existingReferences.length; i++) {
-			if( existingReferences[i].getReferencedComponent() != null ){
-				if(existingReferences[i].getReferencedComponent().getProject().equals(comp.getProject())){
-					return true;
+		if (existingReferences!= null) {
+			for (int i = 0; i < existingReferences.length; i++) {
+				if(existingReferences[i]!=null && existingReferences[i].getReferencedComponent() != null ){
+					if (existingReferences[i].getReferencedComponent().getProject()!=null && comp.getProject()!=null) {
+						if(existingReferences[i].getReferencedComponent().getProject().equals(comp.getProject()))
+							return true;
+					}
 				}
 			}
 		}
