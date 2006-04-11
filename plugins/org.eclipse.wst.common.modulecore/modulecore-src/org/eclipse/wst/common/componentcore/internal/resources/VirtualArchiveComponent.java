fe0bb665c77ff06dc5b37567dcd835d980b4b08d
cbridgha <cbridgha>
2006-04-11 19:19:12 +0000

    [133187] synchronizer fix

--- plugins/org.eclipse.wst.common.modulecore/modulecore-src/org/eclipse/wst/common/componentcore/internal/resources/VirtualArchiveComponent.java
+++ plugins/org.eclipse.wst.common.modulecore/modulecore-src/org/eclipse/wst/common/componentcore/internal/resources/VirtualArchiveComponent.java
@@ -65,7 +65,7 @@ public class VirtualArchiveComponent implements IVirtualComponent, IAdaptable {
 
 		String archivePathString = archiveLocation.substring(4, archiveLocation.length());
 		archiveType	= archiveLocation.substring(0, archiveLocation.length() - archivePathString.length() -1);
-		archivePath = new  Path(archivePathString);
+		archivePath = new  Path(archivePathString).makeRelative();
 	}
 
 	public IVirtualComponent getComponent() {
