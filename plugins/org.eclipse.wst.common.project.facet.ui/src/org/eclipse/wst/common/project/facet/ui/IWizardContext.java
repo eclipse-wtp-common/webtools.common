9e17b01486afc48f6d69cd03c279e4bbc6519d56
kkomissarchik <kkomissarchik>
2006-07-10 20:09:25 +0000

    113137 | Create a tutorial document for facets

--- plugins/org.eclipse.wst.common.project.facet.ui/src/org/eclipse/wst/common/project/facet/ui/IWizardContext.java
+++ plugins/org.eclipse.wst.common.project.facet.ui/src/org/eclipse/wst/common/project/facet/ui/IWizardContext.java
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (c) 2005 BEA Systems, Inc.
+ * Copyright (c) 2005, 2006 BEA Systems, Inc.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -19,6 +19,9 @@ import org.eclipse.wst.common.project.facet.core.IFacetedProject.Action;
 import org.eclipse.wst.common.project.facet.core.IFacetedProject.Action.Type;
 
 /**
+ * The interface exposed to the facet action wizard pages that allows them
+ * to gather information about the wizard state.
+ * 
  * <p><i>This class is part of an interim API that is still under development 
  * and expected to change significantly before reaching stability. It is being 
  * made available at this early stage to solicit feedback from pioneering 
@@ -30,16 +33,64 @@ import org.eclipse.wst.common.project.facet.core.IFacetedProject.Action.Type;
 
 public interface IWizardContext 
 {
+    /**
+     * Returns the name of the project that the wizard is operating on. If the
+     * wizard is in the project creation mode, the project will not yet exist
+     * in the workspace.
+     * 
+     * @return the name of the project that the wizard is operating on
+     */
+    
     String getProjectName();
+    
+    /**
+     * Returns the set of facets currently selected in the wizard. If the wizard
+     * is in the add/remove facets mode (vs. project creation), this method will 
+     * return the set of facets currently installed in a project after being 
+     * modified by the current set of actions. 
+     * 
+     * @return the set of facets currently selected in the wizard (element type:
+     *   {@see IProjectFacetVersion})
+     */
     
     Set getSelectedProjectFacets();
     
+    /**
+     * Determines whether the specified facet is currently selected in the
+     * wizard. See {@see getSelectedProjectFacets()} for more information.
+     * 
+     * @param fv the project facet version object
+     * @return <code>true</code> if an only if the provided project facet is
+     *   currently selected in the wizard
+     */
+    
     boolean isProjectFacetSelected( IProjectFacetVersion fv );
+    
+    /**
+     * Returns the set of actions currently specified by the user.
+     * 
+     * @return the set of actions currently specified by the user
+     */
     
     Set getActions();
     
+    /**
+     * Finds the action of specified type that applies to the specified facet,
+     * if such action exists. If the wizard is in the add/remove facets mode
+     * (vs. project creation), you cannot depend on finding the install action
+     * for a required facet as that facet may have already been installed.
+     * 
+     * @param type the action type
+     * @param fv the project facet version object
+     * @return the action object or <code>null</code>
+     */
+    
     Action getAction( Action.Type type,
-                      IProjectFacetVersion f );
+                      IProjectFacetVersion fv );
+    
+    /**
+     * Do not use! This method is internal and will be removed.
+     */
     
 	Object getConfig(IProjectFacetVersion fv, Type type, String pjname) throws CoreException;
     
