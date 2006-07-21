a0a2fd83665bdc743ff928306682d08a93a3559c
cbridgha <cbridgha>
2006-07-21 14:38:22 +0000

    [151178] ValidateEdit

--- plugins/org.eclipse.wst.common.modulecore/modulecore-src/org/eclipse/wst/common/componentcore/internal/ModuleStructuralModel.java
+++ plugins/org.eclipse.wst.common.modulecore/modulecore-src/org/eclipse/wst/common/componentcore/internal/ModuleStructuralModel.java
@@ -194,7 +194,7 @@ public class ModuleStructuralModel extends EditModel implements IAdaptable {
 	 * @see ResourceStateValidator#checkActivation(ResourceStateValidatorPresenter)
 	 */
 	public void checkActivation(ResourceStateValidatorPresenter presenter) throws CoreException {
-		super.checkActivation(presenter);
+		//super.checkActivation(presenter);
 	}
 	/**
 	 * Subclasses can override - by default this will return the first resource referenced by the
