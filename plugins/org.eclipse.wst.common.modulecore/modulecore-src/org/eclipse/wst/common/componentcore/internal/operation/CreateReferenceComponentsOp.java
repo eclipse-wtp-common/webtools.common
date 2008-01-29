6b84014044a949d71106db292c7b04e1c30223d1
jsholl <jsholl>
2008-01-29 19:23:05 +0000

    [215538] Several problems with J2EE and Web Dependencies properties pages

--- plugins/org.eclipse.wst.common.modulecore/modulecore-src/org/eclipse/wst/common/componentcore/internal/operation/CreateReferenceComponentsOp.java
+++ plugins/org.eclipse.wst.common.modulecore/modulecore-src/org/eclipse/wst/common/componentcore/internal/operation/CreateReferenceComponentsOp.java
@@ -15,9 +15,12 @@ import java.util.List;
 import java.util.Map;
 
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Path;
@@ -67,6 +70,10 @@ public class CreateReferenceComponentsOp extends AbstractDataModelOperation {
 					sEdit.dispose();
 			}
 		}
+		
+		IFile [] files = new IFile[1];
+		files[0] = project.getFile(ProjectUtilities.DOT_PROJECT);
+		status = ResourcesPlugin.getWorkspace().validateEdit(files, null);
 		return status;
 	}
 
@@ -92,13 +99,18 @@ public class CreateReferenceComponentsOp extends AbstractDataModelOperation {
 		IVirtualComponent sourceComp = (IVirtualComponent) model.getProperty(ICreateReferenceComponentsDataModelProperties.SOURCE_COMPONENT);
 		List vlist = new ArrayList();
 		List modList = (List) model.getProperty(ICreateReferenceComponentsDataModelProperties.TARGET_COMPONENT_LIST);
+		String deployPathStr = model.getStringProperty(ICreateReferenceComponentsDataModelProperties.TARGET_COMPONENTS_DEPLOY_PATH);
+		IPath deployPath = null;
+		if (deployPathStr != null && deployPathStr.length() > 0){
+			deployPath = new Path(deployPathStr);
+		}
 		for (int i = 0; i < modList.size(); i++) {
 			IVirtualComponent comp = (IVirtualComponent) modList.get(i);
-			if (!srcComponentContainsReference(sourceComp, comp)) {
+			if (!srcComponentContainsReference(sourceComp, comp, deployPath)) {
 				IVirtualReference ref = ComponentCore.createReference(sourceComp, comp);
-				String deployPath = model.getStringProperty(ICreateReferenceComponentsDataModelProperties.TARGET_COMPONENTS_DEPLOY_PATH);
-				if (deployPath != null && deployPath.length() > 0)
-					ref.setRuntimePath(new Path(deployPath));
+				if(deployPath != null){
+					ref.setRuntimePath(new Path(deployPathStr));
+				}
 
 				String archiveName = getArchiveName(comp);
 				if (archiveName.length() > 0) {
@@ -119,17 +131,21 @@ public class CreateReferenceComponentsOp extends AbstractDataModelOperation {
 	}
 
 
-	private boolean srcComponentContainsReference(IVirtualComponent sourceComp, IVirtualComponent comp) {
+	private boolean srcComponentContainsReference(IVirtualComponent sourceComp, IVirtualComponent comp, IPath deployPath) {
 		if ((sourceComp != null && sourceComp.getProject() != null) && (comp != null && comp.getProject() != null)) {
-		
 			IVirtualReference[] existingReferences = sourceComp.getReferences();
 			IVirtualComponent referencedComponent = null;
 			if (existingReferences != null) {
 				for (int i = 0; i < existingReferences.length; i++) {
-					if (existingReferences[i] != null ) {
-						referencedComponent = existingReferences[i].getReferencedComponent();	
-						if (referencedComponent != null && referencedComponent.equals(comp)  ) 
+					IVirtualReference ref = existingReferences[i];
+					if(ref != null){
+						//also check to see if the deploy path is the same (remember that it can be null)
+						if((ref.getRuntimePath() == null && deployPath != null) || ref.getRuntimePath().equals(deployPath)){
+							referencedComponent = ref.getReferencedComponent();	
+							if (referencedComponent != null && referencedComponent.equals(comp) ){ 
 								return true;
+							}	
+						}
 					}
 				}
 			}
