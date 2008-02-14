9cdd88ff652594b8bc889d7e71c033cf8f6fb027
david_williams <david_williams>
2008-02-14 03:31:56 +0000

    This commit was manufactured by cvs2svn to create tag 'v200802132233'.

--- plugins/org.eclipse.wst.validation/vf2/org/eclipse/wst/validation/internal/ValConstants.java
+++
@@ -1,9 +0,0 @@
-package org.eclipse.wst.validation.internal;
-
-import org.eclipse.wst.validation.internal.plugin.ValidationPlugin;
-
-public interface ValConstants {
-	/** problem marker for version 2 of the validation framework. */
-	String ProblemMarker =  ValidationPlugin.PLUGIN_ID +".problemmarker2"; //$NON-NLS-1$
-
-}
