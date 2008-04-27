dd906bfc3f5bb29101910f6094469a63e465f306
david_williams <david_williams>
2008-04-27 19:59:27 +0000

    fixed copyright via email confirmation from Konstantine, since he's out on vacation.

--- plugins/org.eclipse.wst.common.project.facet.ui/src/org/eclipse/wst/common/project/facet/ui/internal/util/ReadOnlyComboBoxCellEditor.java
+++ plugins/org.eclipse.wst.common.project.facet.ui/src/org/eclipse/wst/common/project/facet/ui/internal/util/ReadOnlyComboBoxCellEditor.java
@@ -1,12 +1,14 @@
-/******************************************************************************
- * Copyright (c) 2008 BEA Systems, Inc.
+/*******************************************************************************
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *    Konstantin Komissarchik - initial implementation and ongoing maintenance
+ *     IBM Corporation - initial API and implementation
+ *     Tom Schindl <tom.schindl@bestsolution.at> - bugfix in 174739
+ *     Konstantin Komissarchik - mproving behavior in the read-only case 
  ******************************************************************************/
 
 package org.eclipse.wst.common.project.facet.ui.internal.util;
