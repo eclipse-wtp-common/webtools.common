0bed1e51c0d7b023a85c6afd01af3097f32fc6d6
david_williams <david_williams>
2008-08-20 01:52:07 +0000

    244086 Build path errors after restart

--- plugins/org.eclipse.wst.common.modulecore/modulecore-src/org/eclipse/wst/common/componentcore/internal/resources/VirtualComponent.java
+++ plugins/org.eclipse.wst.common.modulecore/modulecore-src/org/eclipse/wst/common/componentcore/internal/resources/VirtualComponent.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
 import org.eclipse.core.resources.IProject;
@@ -267,6 +268,10 @@ public class VirtualComponent implements IVirtualComponent {
 		}
 	}	
 
+	public IVirtualReference[] getReferences(Map<String, Object> options) {
+		return getReferences();
+	}
+	
 	public IVirtualReference[] getReferences() { 
 		StructureEdit core = null;
 		List references = new ArrayList();
