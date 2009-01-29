57c783d0fb332cc0f26b8bf5d20633d13033cd9b
gkarasiu <gkarasiu>
2009-01-29 12:14:31 +0000

    [262823] fix versioning error (remove some spaces so the file matches)

--- tests/org.eclipse.wst.common.tests.validation/src/org/eclipse/wst/validation/tests/testcase/TestSuite7.java
+++ tests/org.eclipse.wst.common.tests.validation/src/org/eclipse/wst/validation/tests/testcase/TestSuite7.java
@@ -61,10 +61,9 @@ public class TestSuite7 extends TestCase {
 	 */
 	public void testClean() throws CoreException, UnsupportedEncodingException, InterruptedException {
 		Tracing.log("TestSuite7-01: testClean starting");
-		IProgressMonitor monitor = new NullProgressMonitor();
+		IProgressMonitor monitor = new NullProgressMonitor();	
 		_env.turnOnAutoBuild();
 		_env.cleanBuild(monitor);
-
 		T7A.resetList();
 		
 		_env.cleanBuild(monitor);
