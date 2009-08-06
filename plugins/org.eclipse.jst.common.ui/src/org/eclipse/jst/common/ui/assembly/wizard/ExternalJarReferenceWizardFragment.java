ed165fb713a01d70dd520d4168ed59d0f1bcd02d
cbridgha <cbridgha>
2009-08-06 20:02:26 +0000

    [277482] initial commit for new plugin

---
+++ plugins/org.eclipse.jst.common.ui/src/org/eclipse/jst/common/ui/assembly/wizard/ExternalJarReferenceWizardFragment.java
@@ -0,0 +1,62 @@
+/******************************************************************************
+ * Copyright (c) 2009 Red Hat
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Rob Stryker - initial implementation and ongoing maintenance
+ ******************************************************************************/
+package org.eclipse.jst.common.ui.assembly.wizard;
+
+import java.util.ArrayList;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jdt.ui.wizards.BuildPathDialogAccess;
+import org.eclipse.jst.common.ui.Messages;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.wst.common.componentcore.ComponentCore;
+import org.eclipse.wst.common.componentcore.internal.resources.VirtualArchiveComponent;
+import org.eclipse.wst.common.componentcore.resources.IVirtualComponent;
+import org.eclipse.wst.common.componentcore.ui.propertypage.IReferenceWizardConstants;
+import org.eclipse.wst.server.ui.wizard.IWizardHandle;
+
+public class ExternalJarReferenceWizardFragment extends JarReferenceWizardFragment {
+	public Composite createComposite(Composite parent, IWizardHandle handle) {
+		Composite c = super.createComposite(parent, handle);
+		handle.setTitle(Messages.ExternalJarTitle);
+		handle.setDescription(Messages.ExternalJarDescription);
+		return c;
+	}
+
+	protected void buttonPressed() {
+		selected = BuildPathDialogAccess
+			.chooseExternalJAREntries(browse.getShell());
+		viewer.refresh();
+	}
+
+	public void performFinish(IProgressMonitor monitor) throws CoreException {
+		IVirtualComponent rootComponent = (IVirtualComponent)getTaskModel().getObject(IReferenceWizardConstants.ROOT_COMPONENT);
+		if (selected != null && selected.length > 0) {
+			ArrayList<IVirtualComponent> compList = new ArrayList<IVirtualComponent>();
+			ArrayList<String> paths = new ArrayList<String>();
+			for (int i = 0; i < selected.length; i++) {
+				// IPath fullPath = project.getFile(selected[i]).getFullPath();
+				String type = VirtualArchiveComponent.LIBARCHIVETYPE
+						+ IPath.SEPARATOR;
+				IVirtualComponent archive = ComponentCore
+						.createArchiveComponent(rootComponent.getProject(),
+								type + selected[i].toString());
+				compList.add(archive);
+				paths.add(selected[i].lastSegment());
+			}
+			IVirtualComponent[] components = (IVirtualComponent[]) compList.toArray(new IVirtualComponent[compList.size()]);
+			String[] paths2 = (String[]) paths.toArray(new String[paths.size()]);
+			getTaskModel().putObject(IReferenceWizardConstants.COMPONENT, components);
+			getTaskModel().putObject(IReferenceWizardConstants.COMPONENT_PATH, paths2);
+		}
+	}
+}
