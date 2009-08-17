bc0f21df180670abac0ae2dd33f8b7becf2fcd1a
jsholl <jsholl>
2009-08-17 14:57:13 +0000

    [205094] backing out because JUnit failure/error count is always double and this doesn't work anyway.

--- tests/org.eclipse.wst.common.tests/commontests/org/eclipse/wst/common/tests/BaseTestCase.java
+++ tests/org.eclipse.wst.common.tests/commontests/org/eclipse/wst/common/tests/BaseTestCase.java
@@ -1,16 +1,6 @@
 package org.eclipse.wst.common.tests;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Set;
-
-import junit.framework.AssertionFailedError;
 import junit.framework.TestCase;
-import junit.framework.TestFailure;
-import junit.framework.TestResult;
 
 import org.eclipse.core.runtime.IStatus;
 
@@ -133,122 +123,122 @@ public class BaseTestCase extends TestCase {
 		fFailOnLoggedStatus = failOnLoggedStatus;
 	}
 
-	@Override
-	public void run(TestResult result) {
-		try{
-			AssertWarn.clearWarnings();
-			super.run(result);
-			
-		} finally {
-			List <StackTraceElement> stackTrace = new ArrayList<StackTraceElement>();
-			
-			//check to see if this test already failed with an error
-			//if so add error to stack trace
-			Enumeration errorsEnum = result.errors();
-			TestFailure errorOrFailure = null;
-			boolean hadError = false;
-			for(int i = 0; errorsEnum.hasMoreElements() && !hadError; i++) {
-				Object o = errorsEnum.nextElement();
-				errorOrFailure = (TestFailure)o;
-				if(errorOrFailure.failedTest() == this) {
-					hadError = true;
-					
-					Throwable thrown = errorOrFailure.thrownException();
-					List<StackTraceElement> errorTrace = Arrays.asList(thrown.getStackTrace());
-					stackTrace.addAll(errorTrace);
-				}
-			}
-			
-			//check to see if this test already failed with a failure if it didn't
-			// fail with an error. if so add failure to stack trace
-			boolean hadFailure = false;
-			if(!hadError) {
-				Enumeration failuresEnum = result.failures();
-				for(int i = 0; failuresEnum.hasMoreElements() && !hadFailure; i++) {
-					Object o = failuresEnum.nextElement();
-					errorOrFailure = (TestFailure)o;
-					if(errorOrFailure.failedTest() == this) {
-						hadFailure = true;
-						
-						Throwable thrown = errorOrFailure.thrownException();
-						List<StackTraceElement> failureTrace = Arrays.asList(thrown.getStackTrace());
-						stackTrace.addAll(failureTrace);
-					}
-				}
-			}
-			
-			//add any warnings to the stack trace and deal with if this test already failed
-			List <AssertionFailedError> warnings = AssertWarn.getWarnings();
-			boolean hadWarnings = warnings.size() != 0;
-			if(hadWarnings) {
-				int warningNum = 1;
-				List<StackTraceElement> warningTrace = null;
-				for(AssertionFailedError e : warnings){
-					stackTrace.add(new StackTraceElement("---------->","", "WARNING MESSAGE: " + e.getMessage() + ")  (WARNING NUMBER", warningNum++));
-					
-					warningTrace = Arrays.asList(e.getStackTrace());
-					stackTrace.addAll(warningTrace);
-				}
-			}
-			
-			//add any logged statuses to the stack trace, as long as fFailOnLoggedStatus == true
-			HashMap<IStatus,String> loggedStatuses = JUnitLogListener.INSTANCE.getLoggedStatuses();
-			boolean hadLoggedStatuses = loggedStatuses.size() != 0;
-			if(hadLoggedStatuses && fFailOnLoggedStatus) {
-				int loggedMessageNum = 1;
-				List<StackTraceElement> loggedMessageTrace = null;
-				
-				Set<IStatus> loggedStatusesKeys = loggedStatuses.keySet();
-				String statusType;
-				for(IStatus status : loggedStatusesKeys) {
-					stackTrace.add(new StackTraceElement("---------->","", "LOGGED " + statusTypeToString(status.getSeverity()) + " STATUS: " + loggedStatuses.get(status) + ": " + status.getMessage() + ")  (LOGGED STATUS NUMBER", loggedMessageNum++));
-					
-					if(status.getException() != null) {
-						loggedMessageTrace = Arrays.asList(status.getException().getStackTrace());
-						stackTrace.addAll(loggedMessageTrace);
-					}
-				}
-			}
-			
-			//deal with making a new formated failure and added it to the result
-			String message = "";
-			String warningsMessage = "Test had " + warnings.size() + " warnings occur";
-			String loggedStatusesMessage = "Test logged " + loggedStatuses.size() + " statuses";
-			if(hadError || hadFailure) {
-				message = errorOrFailure.exceptionMessage();
-				
-				if(hadWarnings) {
-					message += "  (" + warningsMessage + ")";
-				}
-				
-				if(hadLoggedStatuses && fFailOnLoggedStatus) {
-					message += "  (" + loggedStatusesMessage + ")";
-				}
-			} else if(hadWarnings) {
-				message = warningsMessage;
-				
-				if(hadLoggedStatuses && fFailOnLoggedStatus) {
-					message += "  (" + loggedStatusesMessage + ")";
-				}
-			} else if(hadLoggedStatuses && fFailOnLoggedStatus) {
-				message = loggedStatusesMessage;
-			}
-			
-			if(hadError || hadFailure || hadWarnings || (hadLoggedStatuses && fFailOnLoggedStatus)) {
-				AssertionFailedError error = new AssertionFailedError(message);
-				StackTraceElement[] stackTraceArray = stackTrace.toArray(new StackTraceElement[stackTrace.size()]);
-				error.setStackTrace(stackTraceArray);
-
-				if(hadError) {
-					result.addError(this, error);
-				} else {
-					result.addFailure(this, error);
-				}
-			}
-
-
-		}
-	}
+//	@Override
+//	public void run(TestResult result) {
+//		try{
+//			AssertWarn.clearWarnings();
+//			super.run(result);
+//			
+//		} finally {
+//			List <StackTraceElement> stackTrace = new ArrayList<StackTraceElement>();
+//			
+//			//check to see if this test already failed with an error
+//			//if so add error to stack trace
+//			Enumeration errorsEnum = result.errors();
+//			TestFailure errorOrFailure = null;
+//			boolean hadError = false;
+//			for(int i = 0; errorsEnum.hasMoreElements() && !hadError; i++) {
+//				Object o = errorsEnum.nextElement();
+//				errorOrFailure = (TestFailure)o;
+//				if(errorOrFailure.failedTest() == this) {
+//					hadError = true;
+//					
+//					Throwable thrown = errorOrFailure.thrownException();
+//					List<StackTraceElement> errorTrace = Arrays.asList(thrown.getStackTrace());
+//					stackTrace.addAll(errorTrace);
+//				}
+//			}
+//			
+//			//check to see if this test already failed with a failure if it didn't
+//			// fail with an error. if so add failure to stack trace
+//			boolean hadFailure = false;
+//			if(!hadError) {
+//				Enumeration failuresEnum = result.failures();
+//				for(int i = 0; failuresEnum.hasMoreElements() && !hadFailure; i++) {
+//					Object o = failuresEnum.nextElement();
+//					errorOrFailure = (TestFailure)o;
+//					if(errorOrFailure.failedTest() == this) {
+//						hadFailure = true;
+//						
+//						Throwable thrown = errorOrFailure.thrownException();
+//						List<StackTraceElement> failureTrace = Arrays.asList(thrown.getStackTrace());
+//						stackTrace.addAll(failureTrace);
+//					}
+//				}
+//			}
+//			
+//			//add any warnings to the stack trace and deal with if this test already failed
+//			List <AssertionFailedError> warnings = AssertWarn.getWarnings();
+//			boolean hadWarnings = warnings.size() != 0;
+//			if(hadWarnings) {
+//				int warningNum = 1;
+//				List<StackTraceElement> warningTrace = null;
+//				for(AssertionFailedError e : warnings){
+//					stackTrace.add(new StackTraceElement("---------->","", "WARNING MESSAGE: " + e.getMessage() + ")  (WARNING NUMBER", warningNum++));
+//					
+//					warningTrace = Arrays.asList(e.getStackTrace());
+//					stackTrace.addAll(warningTrace);
+//				}
+//			}
+//			
+//			//add any logged statuses to the stack trace, as long as fFailOnLoggedStatus == true
+//			HashMap<IStatus,String> loggedStatuses = JUnitLogListener.INSTANCE.getLoggedStatuses();
+//			boolean hadLoggedStatuses = loggedStatuses.size() != 0;
+//			if(hadLoggedStatuses && fFailOnLoggedStatus) {
+//				int loggedMessageNum = 1;
+//				List<StackTraceElement> loggedMessageTrace = null;
+//				
+//				Set<IStatus> loggedStatusesKeys = loggedStatuses.keySet();
+//				String statusType;
+//				for(IStatus status : loggedStatusesKeys) {
+//					stackTrace.add(new StackTraceElement("---------->","", "LOGGED " + statusTypeToString(status.getSeverity()) + " STATUS: " + loggedStatuses.get(status) + ": " + status.getMessage() + ")  (LOGGED STATUS NUMBER", loggedMessageNum++));
+//					
+//					if(status.getException() != null) {
+//						loggedMessageTrace = Arrays.asList(status.getException().getStackTrace());
+//						stackTrace.addAll(loggedMessageTrace);
+//					}
+//				}
+//			}
+//			
+//			//deal with making a new formated failure and added it to the result
+//			String message = "";
+//			String warningsMessage = "Test had " + warnings.size() + " warnings occur";
+//			String loggedStatusesMessage = "Test logged " + loggedStatuses.size() + " statuses";
+//			if(hadError || hadFailure) {
+//				message = errorOrFailure.exceptionMessage();
+//				
+//				if(hadWarnings) {
+//					message += "  (" + warningsMessage + ")";
+//				}
+//				
+//				if(hadLoggedStatuses && fFailOnLoggedStatus) {
+//					message += "  (" + loggedStatusesMessage + ")";
+//				}
+//			} else if(hadWarnings) {
+//				message = warningsMessage;
+//				
+//				if(hadLoggedStatuses && fFailOnLoggedStatus) {
+//					message += "  (" + loggedStatusesMessage + ")";
+//				}
+//			} else if(hadLoggedStatuses && fFailOnLoggedStatus) {
+//				message = loggedStatusesMessage;
+//			}
+//			
+//			if(hadError || hadFailure || hadWarnings || (hadLoggedStatuses && fFailOnLoggedStatus)) {
+//				AssertionFailedError error = new AssertionFailedError(message);
+//				StackTraceElement[] stackTraceArray = stackTrace.toArray(new StackTraceElement[stackTrace.size()]);
+//				error.setStackTrace(stackTraceArray);
+//
+//				if(hadError) {
+//					result.addError(this, error);
+//				} else {
+//					result.addFailure(this, error);
+//				}
+//			}
+//
+//
+//		}
+//	}
 	
 	private String statusTypeToString(int statusType) {
 		String sStatusType = "";
