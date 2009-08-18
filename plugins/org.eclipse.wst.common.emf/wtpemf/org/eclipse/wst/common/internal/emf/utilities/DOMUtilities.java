4cc73093ec1d56d77bf5b32ce22b568e2dd0bf44
canderson <canderson>
2009-08-18 18:20:29 +0000

    [286871] should disable deferred DOM expansion in parser used by DOMUtilities

--- plugins/org.eclipse.wst.common.emf/wtpemf/org/eclipse/wst/common/internal/emf/utilities/DOMUtilities.java
+++ plugins/org.eclipse.wst.common.emf/wtpemf/org/eclipse/wst/common/internal/emf/utilities/DOMUtilities.java
@@ -502,6 +502,11 @@ public class DOMUtilities {
 			dbf.setValidating(false);
 			Logger.getLogger().log("Warning: Parser does not support \"http://apache.org/xml/features/validation/schema\". Validation will be disabled."); //$NON-NLS-1$
 		}
+		try {
+			dbf.setAttribute("http://apache.org/xml/features/dom/defer-node-expansion", Boolean.FALSE);
+		} catch (IllegalArgumentException ignore) {
+			Logger.getLogger().log("Warning: Parser does not support \"http://apache.org/xml/features/dom/defer-node-expansion\"."); //$NON-NLS-1$
+		}
 		dbf.setExpandEntityReferences(options.isExpandEntityRefererences());
 		DocumentBuilder result = dbf.newDocumentBuilder();
 		result.setErrorHandler(new ErrorHandler() {
