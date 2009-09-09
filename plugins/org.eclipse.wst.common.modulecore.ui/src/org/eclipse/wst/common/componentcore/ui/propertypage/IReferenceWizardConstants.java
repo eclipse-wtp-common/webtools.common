e2722bc799f79bbf178e1de0a8da81c06d4491d0
david_williams <david_williams>
2009-09-09 21:26:26 +0000

    This commit was manufactured by cvs2svn to create tag 'v200909100430'.

--- plugins/org.eclipse.wst.common.modulecore.ui/src/org/eclipse/wst/common/componentcore/ui/propertypage/IReferenceWizardConstants.java
+++
@@ -1,8 +0,0 @@
-package org.eclipse.wst.common.componentcore.ui.propertypage;
-
-public interface IReferenceWizardConstants {
-	public static final String COMPONENT = "dependency.component"; //$NON-NLS-1$
-	public static final String COMPONENT_PATH = "dependency.component.path"; //$NON-NLS-1$
-	public static final String PROJECT = "root.project"; //$NON-NLS-1$
-	public static final String ROOT_COMPONENT = "root.component"; //$NON-NLS-1$
-}
