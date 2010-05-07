c84af4734c23cba9bf155b59e80ec8e0c77914ba
canderson <canderson>
2010-05-07 01:35:25 +0000

    [311097] [NLS] validation builder job uses incorrect message

--- plugins/org.eclipse.wst.validation/vf2/org/eclipse/wst/validation/internal/ValBuilderJob.java
+++ plugins/org.eclipse.wst.validation/vf2/org/eclipse/wst/validation/internal/ValBuilderJob.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2008 IBM Corporation and others.
+ * Copyright (c) 2007, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,6 +10,7 @@
  *******************************************************************************/
 package org.eclipse.wst.validation.internal;
 
+import java.text.MessageFormat;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -161,7 +162,7 @@ public final class ValBuilderJob extends WorkspaceJob {
 	}
 
 	private void run(ValidationRequest request, IProgressMonitor monitor){
-		setName(ValMessages.JobName + " " + request.getProject().getName()); //$NON-NLS-1$
+		setName(MessageFormat.format(ValMessages.JobNameWithProjectName,new Object[] { request.getProject().getName() })); 
 		try {		
 	        IValidatorVisitor startingVisitor = new IValidatorVisitor(){
 
--- plugins/org.eclipse.wst.validation/vf2/org/eclipse/wst/validation/internal/ValMessages.java
+++ plugins/org.eclipse.wst.validation/vf2/org/eclipse/wst/validation/internal/ValMessages.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2009 IBM Corporation and others.
+ * Copyright (c) 2007, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -43,6 +43,7 @@ public class ValMessages extends NLS {
 	public static String GroupExclude;
 	
 	public static String JobName;
+	public static String JobNameWithProjectName;
 	public static String JobNameMonitor;
 	
 	public static String JobIndexSave;
