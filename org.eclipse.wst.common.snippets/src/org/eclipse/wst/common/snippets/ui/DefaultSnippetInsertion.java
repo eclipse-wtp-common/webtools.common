9fbe3920d396f075ad6e8a1b00b0ce0b40999e5b
nitind <nitind>
2011-01-13 04:42:13 +0000

    [334176] Canceling the variable insertion dialog causes errors

--- org.eclipse.wst.common.snippets/src/org/eclipse/wst/common/snippets/ui/DefaultSnippetInsertion.java
+++ org.eclipse.wst.common.snippets/src/org/eclipse/wst/common/snippets/ui/DefaultSnippetInsertion.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2006 IBM Corporation and others.
+ * Copyright (c) 2004, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -139,7 +139,7 @@ public class DefaultSnippetInsertion implements ISnippetInsertion {
 			if (!"\n".equals(systemEOL) && systemEOL != null) { //$NON-NLS-1$
 				content = StringUtils.replace(content, "\n", systemEOL); //$NON-NLS-1$
 			}
-			event.data = content;
+			event.data = content != null ? content : " "; //$NON-NLS-1$ // TextTransfer does not like non-empty strings
 		}
 		else {
 			/*
