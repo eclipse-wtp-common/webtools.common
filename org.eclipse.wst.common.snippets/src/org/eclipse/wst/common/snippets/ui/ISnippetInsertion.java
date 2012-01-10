0cebd0c92ec746e7bad3172ecfcad62581c85c29
nitind <nitind>
2012-01-10 20:17:46 +0000

    [223780] Adopt PDE API Tooling

--- org.eclipse.wst.common.snippets/src/org/eclipse/wst/common/snippets/ui/ISnippetInsertion.java
+++ org.eclipse.wst.common.snippets/src/org/eclipse/wst/common/snippets/ui/ISnippetInsertion.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2006 IBM Corporation and others.
+ * Copyright (c) 2005, 2012 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -24,6 +24,7 @@ import org.eclipse.wst.common.snippets.core.ISnippetItem;
  * Clients may implement this interface.
  * </p>
  * @since 1.0
+ * @noimplement This interface is not intended to be implemented by clients.
  */
 public interface ISnippetInsertion {
 
