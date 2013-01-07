b691b73eba1519a421f16fe10babe0e39bd2c6a6
Rosendo Martinez <rosendo@mx1.ibm.com>
2013-01-07 11:34:47 -0600

    [380173] Validation rule for target runtime is not working on EJB Validator.

--- plugins/org.eclipse.wst.validation/vf2/org/eclipse/wst/validation/internal/model/FilterRule.java
+++ plugins/org.eclipse.wst.validation/vf2/org/eclipse/wst/validation/internal/model/FilterRule.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2011 IBM Corporation and others.
+ * Copyright (c) 2007, 2012 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -567,6 +567,16 @@ public abstract class FilterRule implements IAdaptable {
 		
 		public String getDisplayableType() {
 			return ValMessages.RuleTargetRuntime;
+		}
+		
+		public Boolean matchesProject(IProject project){
+			try {
+				return ValidatorHelper.getTargetRuntime(project).getName().equals(_pattern);
+			} catch (CoreException e) {
+				//do nothing
+			}
+			
+			return false;
 		}
 		
 		@Override
