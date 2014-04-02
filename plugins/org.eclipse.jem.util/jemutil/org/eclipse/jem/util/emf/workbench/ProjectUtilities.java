38dd06e93aca083524f868a76b6f5c3514996ebd
Roberto Sanchez <rsanchez@mx1.ibm.com>
2014-04-02 11:57:08 -0600

    [431265] NPE in org.eclipse.jem.util.emf.workbench.ProjectUtilities.createFileURL

--- plugins/org.eclipse.jem.util/jemutil/org/eclipse/jem/util/emf/workbench/ProjectUtilities.java
+++ plugins/org.eclipse.jem.util/jemutil/org/eclipse/jem/util/emf/workbench/ProjectUtilities.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005 IBM Corporation and others.
+ * Copyright (c) 2005, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials 
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -666,12 +666,15 @@ public class ProjectUtilities {
 	}
 
 	protected static URL createFileURL(IPath path) {
-		try {
-			return path.toFile().toURL();
-		} catch (MalformedURLException e) {
-			Logger.getLogger().log(e, Level.WARNING);
-			return null;
+		if (path != null){
+			try {
+				return path.toFile().toURL();
+			} catch (MalformedURLException e) {
+				Logger.getLogger().log(e, Level.WARNING);
+				return null;
+			}
 		}
+		return null;
 	}
 
 	/**
