294553e79d8ab8193bd0fa9e29aebb9ca62322aa
Brian Vosburgh <brian.vosburgh@oracle.com>
2015-01-14 12:10:59 -0600

    [362114]  LibraryInstallPanel triggers SWTException

--- plugins/org.eclipse.jst.common.project.facet.ui/src/org/eclipse/jst/common/project/facet/ui/libprov/internal/LibraryInstallPanel.java
+++ plugins/org.eclipse.jst.common.project.facet.ui/src/org/eclipse/jst/common/project/facet/ui/libprov/internal/LibraryInstallPanel.java
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (c) 2010 Oracle
+ * Copyright (c) 2010, 2015 Oracle and others
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -202,6 +202,14 @@ public final class LibraryInstallPanel
                                                final Object oldValue,
                                                final Object newValue )
     {
+    	if(  isDisposed()  )
+    		
+    	{
+    		
+    		return;
+    		
+    	}
+    	
         if( getDisplay().getThread() != Thread.currentThread() )
         {
             getDisplay().asyncExec
