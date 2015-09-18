37fe0fcf6d7d925cc974b19ebfe585ff424f2116
Carl Anderson <ccc@us.ibm.com>
2015-09-18 18:05:15 -0400

    [477861] Remove dependency on org.eclipse.core.runtime.compatibility bundle

---
+++ tests/org.eclipse.wst.common.tests.collector/collector/org/eclipse/wst/common/tests/collector/TestCollectorPlugin.java
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Copyright (c) 2015 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.wst.common.tests.collector;
+
+import org.eclipse.core.runtime.IExtensionPoint;
+import org.eclipse.core.runtime.IPluginDescriptor;
+import org.eclipse.core.runtime.Plugin;
+
+
+public class TestCollectorPlugin extends Plugin {
+	public static String PLUGIN_ID = "org.eclipse.wst.common.tests.collector";
+	public static TestCollectorPlugin instance = null;
+	public IExtensionPoint dataModelVerifierExt = null;
+	
+	/**
+	 * @param descriptor
+	 */
+	public TestCollectorPlugin(IPluginDescriptor descriptor) {
+		super(descriptor);
+		instance = this;
+		dataModelVerifierExt = descriptor.getExtensionPoint("DataModelVerifier");
+	}
+
+}
