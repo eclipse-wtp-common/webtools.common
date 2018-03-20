27886aea837266c3dbb6a1e2562b5627a1e01af9
John Collier <John.J.Collier@ibm.com>
2018-03-20 13:09:27 -0400

    Bug 532353: Add Java 10 support
    Adds suppport for Java 10 by adding the Java 10 facet to
    org.eclipse.jst.common.project.facet.core.
    
    Signed-off-by: John Collier <John.J.Collier@ibm.com>

--- plugins/org.eclipse.jst.common.project.facet.core/src/org/eclipse/jst/common/project/facet/core/internal/JavaFacetUtil.java
+++ plugins/org.eclipse.jst.common.project.facet.core/src/org/eclipse/jst/common/project/facet/core/internal/JavaFacetUtil.java
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (c) 2010, 2017 Oracle
+ * Copyright (c) 2010, 2018 Oracle
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,6 +8,7 @@
  * Contributors:
  *    Konstantin Komissarchik - initial implementation and ongoing maintenance
  *    Carl Anderson - Java 9 support
+ *    John Collier - Java 10 support
  ******************************************************************************/
 
 package org.eclipse.jst.common.project.facet.core.internal;
@@ -63,6 +64,7 @@ public final class JavaFacetUtil
         FACET_VER_TO_EXEC_ENV.put( JavaFacet.VERSION_1_7, "JavaSE-1.7" ); //$NON-NLS-1$
         FACET_VER_TO_EXEC_ENV.put( JavaFacet.VERSION_1_8, "JavaSE-1.8" ); //$NON-NLS-1$
         FACET_VER_TO_EXEC_ENV.put( JavaFacet.VERSION_9, "JavaSE-9" ); //$NON-NLS-1$
+        FACET_VER_TO_EXEC_ENV.put( JavaFacet.VERSION_10, "JavaSE-10" ); //$NON-NLS-1$
     }
     
     public static String getCompilerLevel()
