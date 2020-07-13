8c4c72cb286a4e042a6ba7ec794e1a6659adf168
nickboldt <nboldt@redhat.com>
2020-07-13 13:15:25 -0400

    Fix license text (1.0 -> 2.0) via patch contributed by Sudip Mukherjee <sudipm.mukherjee@gmail.com>
    Signed-off-by: nickboldt <nboldt@redhat.com>

--- plugins/org.eclipse.wst.common.frameworks.ui/wtp_ui/org/eclipse/wst/common/frameworks/internal/ui/InternalCommonWorkbenchMessages.java
+++ plugins/org.eclipse.wst.common.frameworks.ui/wtp_ui/org/eclipse/wst/common/frameworks/internal/ui/InternalCommonWorkbenchMessages.java
@@ -1,7 +1,7 @@
 /**********************************************************************
  * Copyright (c) 2006 IBM Corporation and others. All rights reserved.   This
  * program and the accompanying materials are made available under the terms of
- * the Eclipse Public License v1.0 which accompanies this distribution, and is
+ * the Eclipse Public License v2.0 which accompanies this distribution, and is
  * available at https://www.eclipse.org/legal/epl-2.0/
 
  * 
--- plugins/org.eclipse.wst.common.modulecore/modulecore-src/org/eclipse/wst/common/componentcore/internal/resources/AbstractResourceListVirtualComponent.java
+++ plugins/org.eclipse.wst.common.modulecore/modulecore-src/org/eclipse/wst/common/componentcore/internal/resources/AbstractResourceListVirtualComponent.java
@@ -2,7 +2,7 @@
  * Copyright (c) 2009 Red Hat, Inc. 
  * Distributed under license by Red Hat, Inc. All rights reserved. 
  * This program is made available under the terms of the 
- * Eclipse Public License v1.0 which accompanies this distribution, 
+ * Eclipse Public License v2.0 which accompanies this distribution, 
  * and is available at https://www.eclipse.org/legal/epl-2.0/
  
  * 
